ba6243edc6e099c005eef8464558842b18c65980
Schuyler Goodwin <schuylergoodwin@gmail.com>
2013-05-05 17:07:25 -0400

    Peds: added methods and images

--- ASquirrel'sTale/src/edu/elon/cs/squirrelgame/Pedestrian.java
+++ ASquirrel'sTale/src/edu/elon/cs/squirrelgame/Pedestrian.java
@@ -13,38 +13,51 @@ public class Pedestrian {
 	protected float centerX;
 	protected float centerY;
 	private float width, height;
-	private Bitmap ped;
+	private Bitmap image;
 	private float currentX;
 	private float currentY;
-	private float currentBallX;
-	private float currentBallY;
+	private float currentHeroX;
+	private float currentHeroY;
 	private int screenWidth;
 	private int screenHeight;
 	private Context context; 
+	private int acornCost; 
+	private int imageID; 
 	
-	private final float FACTOR = 2.7f;
+	private final float FACTOR = 4f;
 	private final int DIFFICULTY_LEVEL = 10;
 	
-	public Pedestrian(Context context /*, appearance, acornCost*/){
-		ped = BitmapFactory.decodeResource(context.getResources(), R.drawable.checkered);
-		width = ped.getWidth()/FACTOR;
-		height = ped.getHeight()/FACTOR;
+	public Pedestrian(Context context, int imageID, int acornCost){
+		this.imageID = imageID;
+		image = BitmapFactory.decodeResource(context.getResources(), imageID);
+		width = image.getWidth()/FACTOR;
+		height = image.getHeight()/FACTOR;
 		centerX = width/FACTOR+60;
 		centerY = height/FACTOR+60;
 		this.context = context; 
-		
+		this.acornCost = acornCost; 
 	}
 	
 	public Pedestrian clone(float xPos, float yPos){
-		 Pedestrian cloned = new Pedestrian(context); 
+		 Pedestrian cloned = new Pedestrian(context, imageID, acornCost); 
 		 cloned.currentX = xPos;
 		 cloned.currentY = yPos; 
+		 return cloned; 
+	}
+	
+	public Pedestrian clone(){
+		 Pedestrian cloned = new Pedestrian(context, imageID, acornCost); 
 		 return cloned; 
 		
 	}
 	
+	public void die(){
+		currentX = screenWidth * -2;
+		currentY = screenHeight * -2;  
+	}
+	
 	public void doDraw(Canvas canvas){
-		canvas.drawBitmap(ped, null, 
+		canvas.drawBitmap(image, null, 
 				new Rect((int)(centerX-width/FACTOR),(int)(centerY-height/FACTOR),
 				(int)(centerX+width/FACTOR),(int)(centerY+height/FACTOR)),null);
 		
@@ -53,11 +66,12 @@ public class Pedestrian {
 	}
 	
 	public void update(double elapsed, float ballX, float ballY){
-		currentBallX = ballX;
-		currentBallY = ballY;
+		currentHeroX = ballX;
+		currentHeroY = ballY;
 				
-		if((currentBallX >= centerX-DIFFICULTY_LEVEL && currentBallX <= centerX+DIFFICULTY_LEVEL) 
-				&& (currentBallY >= centerY-DIFFICULTY_LEVEL && currentBallY <= centerY+DIFFICULTY_LEVEL) ){
+		//decides if hero is 'on top' of them 
+		if((currentHeroX >= centerX-DIFFICULTY_LEVEL && currentHeroX <= centerX+DIFFICULTY_LEVEL) 
+				&& (currentHeroY >= centerY-DIFFICULTY_LEVEL && currentHeroY <= centerY+DIFFICULTY_LEVEL) ){
 			currentX =Math.abs((float) Math.random() * ((screenWidth - width) - width) + width);
 			centerX = currentX;
 			currentY = Math.abs((float) Math.random() * ((screenHeight - height) - height) + height);
